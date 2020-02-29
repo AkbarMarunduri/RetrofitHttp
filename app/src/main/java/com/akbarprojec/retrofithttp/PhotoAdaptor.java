fb6c65c40082f3d67b9e43d415a91a082bfd7bf0
AkbarMarunduri <55292439+AkbarMarunduri@users.noreply.github.com>
2020-03-01 01:39:28 +0700

    PhotoAdaptor ok sisa MainActivity

--- app/src/main/java/com/akbarprojec/retrofithttp/PhotoAdaptor.java
+++ app/src/main/java/com/akbarprojec/retrofithttp/PhotoAdaptor.java
@@ -7,6 +7,9 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jakewharton.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -14,10 +17,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class PhotoAdaptor extends RecyclerView.Adapter<PhotoAdaptor.PhotoViewHolder> {
 
-    private List<PhotoAdaptor> allPhoto;
+    private List<PhotoData> allPhoto;
     private Context context;
 
-    public PhotoAdaptor(List<PhotoAdaptor> allPhoto, Context context) {
+    public PhotoAdaptor(List<PhotoData> allPhoto, Context context) {
         this.allPhoto = allPhoto;
         this.context = context;
     }
@@ -26,12 +29,17 @@ public class PhotoAdaptor extends RecyclerView.Adapter<PhotoAdaptor.PhotoViewHol
     public PhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.card_view_photo, parent, false);
-        return new PhotoAdaptor(view);
+        return new PhotoViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PhotoAdaptor.PhotoViewHolder holder, int position) {
-
+        Picasso.Builder builder = new Picasso.Builder(context);
+        builder.downloader(new OkHttp3Downloader(context));
+        builder.build().load(allPhoto.get(position).getTubnailUrl())
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .into(holder.converImageView);
     }
 
     @Override
